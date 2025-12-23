bc3b73c70a6f814c8e49a735360063080ca50e95
Athish <cathishk@gmail.com>
2025-12-23 21:28:33 +0530

    card

--- react-first-project/src/App.jsx
+++ react-first-project/src/App.jsx
@@ -1,18 +1,12 @@
-import Footer from "./Footer.jsx"
-import Header from "./Header.jsx"
-import Food from "./Food.jsx"
-/*  Components --- A component is a small section of code that can include a JavaScript and HTML. Your function will return that code and make it reusable.
-    JSX -- JS XML -- to combines java script and html
-    {}  inside write js variables see Food.jsx file*/ 
-function App()
-{
-  return(
+import Card from "./Card.jsx";
+function App() {
+  return (
     <>
-      <Header/>
-      <Food/>
-      <Food/>
-      <Footer/>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </>
-  )
+  );
 }
-export default App
+export default App;
--- react-first-project/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <>
-      <p>&copy; 2026 My website</p>
-      <hr></hr>
-    </>
-  );
-}
-export default Footer;
