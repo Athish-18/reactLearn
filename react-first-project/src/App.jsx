4f1edd9fddc2570e485b1e2c1abd81b7be24ea84
Athish <cathishk@gmail.com>
2025-12-24 19:25:53 +0530

    conditional rendering

--- react-first-project/src/App.jsx
+++ react-first-project/src/App.jsx
@@ -1,14 +1,8 @@
-/*
-These are props. They are read-only properties that are shared between components. A parent component can send data to a child component., default props enable to take those values when not sent , proptypes ensure u pass in crct datatype*/ 
-import Student from "./Student.jsx"
+import User from "./User.jsx";
 function App() {
   return (
     <>
-      <Student name="john" age={30} isStudent={true}></Student>
-      <Student name="cena" age={30} isStudent={true}></Student>
-      <Student name="seth" age={30} isStudent={true}></Student>
-      <Student name="30" age={30} isStudent={true}></Student>
-      <Student ></Student>
+      <User isUser={true} name="Athish"></User>
     </>
   );
 }
