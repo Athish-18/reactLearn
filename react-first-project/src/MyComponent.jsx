f675b913c21a8b0c3b670c24138873a2e8b2028d
Athish <cathishk@gmail.com>
2025-12-26 21:17:28 +0530

    update arrays in state

--- react-first-project/src/MyComponent.jsx
+++ react-first-project/src/MyComponent.jsx
@@ -1,53 +1,39 @@
 import React, { useState } from "react";
 
 function MyComponent() {
-  // State object to store car details
-  const [car, setCar] = useState({
-    year: 2025,
-    make: "Renault",
-    model: "SUV",
-  });
+  // State to store list of foods
+  const [foods, setFoods] = useState(["Pizza", "Burger", "Hotdog"]);
 
-  // Update only the 'year' property
-  // Uses updater function to safely access previous state
-  const handleYear = (event) => {
-    setCar((c) => ({
-      ...c, // spread previous state
-      year: event.target.value, // update year
-    }));
-  };
+  // Function to add a new food item
+  const handleAddFood = () => {
+    // Get input value
+    const foodItem = document.getElementById("fav").value;
 
-  // Update only the 'make' property
-  const handleMake = (event) => {
-    setCar((c) => ({
-      ...c, // spread previous state
-      make: event.target.value, // update make
-    }));
-  };
+   
 
-  // Update only the 'model' property
-  const handleModel = (event) => {
-    setCar((c) => ({
-      ...c, // spread previous state
-      model: event.target.value, // update model
-    }));
+    // Update state using updater function
+    setFoods((f) => [...f, foodItem]);
+
+    // Clear input field after adding
+    document.getElementById("fav").value = "";
   };
 
   return (
     <>
-      {/* Display car details */}
-      <p>
-        Your Fav Car is {car.make} {car.model} {car.year}
-      </p>
+      <h1>My Fav Foods</h1>
 
-      {/* Input to update year */}
-      <input type="number" value={car.year} onChange={handleYear} />
+      <ul>
+        {/* map() must RETURN JSX */}
+        {foods.map((food, index) => (
+          <li key={index}>{food}</li>
+        ))}
+      </ul>
 
-      {/* Input to update make */}
-      <input type="text" value={car.make} onChange={handleMake} />
+      {/* Input field */}
+      <input type="text" id="fav" placeholder="Type favorite food" />
 
-      {/* Input to update model */}
-      <input type="text" value={car.model} onChange={handleModel} />
+      {/* Button to add food */}
+      <button onClick={handleAddFood}>Add food</button>
     </>
   );
 }
